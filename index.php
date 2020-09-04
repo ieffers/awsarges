e71ccee3aa9534762d69ded6557460734426ce11
ieffers <jasanchezg91@gmail.com>
2020-09-04 15:45:43 +0200

    Empezando el crud

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
 </head>
 <body>
     <h1>Bienvenido a mi pagina en AWS!</h1>
-    <form class="formulario">
+    <form action='registro.php' method='POST' class="formulario">
         <h4>Formulario de Registro</h4>
         <input class= "datosformulario" type="text" name="username" id="usermane" placeholder="Introduzca su ID">
         <input class= "datosformulario" type="text" name="firstname" id="firstname" placeholder="Introduzca su nombre">
         <input class= "datosformulario" type="password" name="password" id="password" placeholder="Introduzca su contraseña">
+        <input class= "datosformulario" type="password" name="password" id="repassword" placeholder="Repita su contraseña">
         <input class= "submit" type="submit" value="Enviar">
     </form>
 </body>
---
+++ registro.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'Has llegado hasta aqui'
+?>
