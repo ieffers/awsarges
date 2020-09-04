48e444ea852dae626f7d716a7086b0a1677561c9
ieffers <jasanchezg91@gmail.com>
2020-09-04 15:52:12 +0200

    Añadiendo required

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
     <h1>Bienvenido a mi pagina en AWS!</h1>
     <form action='registro.php' method='POST' class="formulario">
         <h4>Formulario de Registro</h4>
-        <input class= "datosformulario" type="text" name="username" id="usermane" placeholder="Introduzca su ID">
-        <input class= "datosformulario" type="text" name="firstname" id="firstname" placeholder="Introduzca su nombre">
-        <input class= "datosformulario" type="password" name="password" id="password" placeholder="Introduzca su contraseña">
-        <input class= "datosformulario" type="password" name="password" id="repassword" placeholder="Repita su contraseña">
+        <input class= "datosformulario" type="text" name="username" id="usermane" placeholder="Introduzca su ID" required>
+        <input class= "datosformulario" type="text" name="firstname" id="firstname" placeholder="Introduzca su nombre" required>
+        <input class= "datosformulario" type="password" name="password" id="password" placeholder="Introduzca su contraseña" required>
+        <input class= "datosformulario" type="password" name="password" id="repassword" placeholder="Repita su contraseña" required>
         <input class= "submit" type="submit" value="Enviar">
     </form>
 </body>
