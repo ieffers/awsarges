c5f2051dfb72a706ed972b98ccbb77594619ef47
ieffers <jasanchezg91@gmail.com>
2020-11-23 22:34:37 +0100

    añadiendo bootstrap

--- assets/menu.php
+++ assets/menu.php
@@ -1,11 +1,18 @@
 <body>
     <header>
-    <ul class="menu">
-            <li><a href="index.php"> Inicio </a></li>
-            <li><a href="formularioRegistro.php"> Registro </a></li>
-            <li><a href="#"> Quienes Somos </a></li>
-            <li><a href="#"> Contacto </a></li>
-            <li><a href="#"> Blog </a></li>
-            <li><a href="formularioComentario.php"> Dejar Comentario </a></li>
-    </ul>
-</header>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="container">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a class="nav-link" href="index.php"> Inicio </a></li>
+                        <li class="nav-item"><a class="nav-link" href="formularioRegistro.php"> Registro </a></li>
+                        <li class="nav-item"><a class="nav-link" href="#"> Quienes Somos </a></li>
+                        <li class="nav-item"><a class="nav-link" href="#"> Contacto </a></li>
+                        <li class="nav-item"><a class="nav-link" href="#"> Blog </a></li>
+                        <li class="nav-item"><a class="nav-link" href="formularioComentario.php"> Dejar Comentario </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -2,27 +2,25 @@
     require_once ('/var/www/html/assets/header.php');
     require_once ('/var/www/html/assets/menu.php');
 ?>
-
-
     <div class="container">
-    
-        <hr>
         <main>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae asperiores dolore officiis necessitatibus iure quia hic quod amet, natus saepe nesciunt incidunt molestiae blanditiis harum quaerat suscipit minus facere.<p>
+            <p><small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae asperiores dolore officiis necessitatibus iure quia hic quod amet, natus saepe nesciunt incidunt molestiae blanditiis harum quaerat suscipit minus facere. </small> <p>
             <div>
                 <img src="assets\img\main.jpg" alt="main">
             </div>
         </main>
         <aside>
-        <h3>Redes Sociales</h3>
-        <ul>
-            <li><a href="https://www.facebook.com/" target="_blank"> <img src="assets\icons\facebook.png" alt="facebook"> </a></li>
-            <li><a href="https://www.instagram.com/" target="_blank"> <img src="assets\icons\instagram.png" alt="instagram">  </a></li>
-            <li><a href="https://www.twitter.com/" target="_blank"> <img src="assets\icons\twitter.png" alt="twitter"> </a></li>
+        <div class="container-sm">
+        <h4><small>Redes Sociales</small></h4>
+        <ul class="navbar-nav mr-auto">
+            <li><a class="nav-item" href="https://www.facebook.com/" target="_blank" alt="facebook"> <img src="assets\icons\facebook.png" alt="facebook"> </a></li>
+            <li><a class="nav-item" href="https://www.instagram.com/" target="_blank"> <img src="assets\icons\instagram.png" alt="instagram">  </a></li>
+            <li><a class="nav-item" href="https://www.twitter.com/" target="_blank"> <img src="assets\icons\twitter.png" alt="twitter"> </a></li>
         </ul>
-    </aside>
+        </div>
+        </aside>
     </div>
-    
-    <?php
+
+<?php
     require_once ('/var/www/html/assets/footer.php');
 ?>
