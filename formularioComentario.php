e07206ce68a217f5f986ec333778b7011d367ef6
ieffers <jasanchezg91@gmail.com>
2020-11-24 07:18:53 +0100

    primer arreglo formulario

--- formularioComentario.php
+++ formularioComentario.php
@@ -5,24 +5,26 @@
     require_once ('/var/www/html/php/comments/lastfive.php');
 ?>
     <div class="container">
+    <div class="form-group">
     <form action="/php/comments/registro.php" method="POST" class="formulario fixed">
-        <h2> Crear comentario </h2>
+        <h3> Crear comentario </h3>
         <input class="datosformulario" type="text" name="username" id="usnermane" placeholder= "Introduzca su nombre">
-        <textarea name="comment" id="comment" rows="4" cols="8"  maxlength=140 placeholder="Introduzca su comentario, maximo 140" ></textarea>
+        <textarea class="md-textarea" name="comment" id="comment" rows="4" cols="8"  maxlength=140 placeholder="Introduzca su comentario, maximo 140" ></textarea>
         <input class="submit" type="submit" value="Enviar">
     </form>
+    </div>
     <form  class="formulario">
-        <h2> Ultimos 5 Comentarios </h2>
+        <h3> Ultimos 5 Comentarios </h3>
         <?php
 
         foreach ($readArray as $key => $ArrayData){
-             echo "<input class='datosformulario' type='text' value='$ArrayData' readonly >";
+             echo "<input class='datosformulario' type='text' value='$ArrayData' >";
             }
         ?>
 
     </form>
     <div class="formulario comentario">
-        <h2> Ultimo comentario <h2>
+        <h3> Ultimo comentario <h3>
         <input class="datosformulario" type="text" value="<?php echo  $read['username'] ?>">
         <input class="datosformulario" type="text" value="<?php echo  $read['date'] ?>"> 
         <input class="datosformulario" type="text" value="<?php echo  $read['comment'] ?>">
